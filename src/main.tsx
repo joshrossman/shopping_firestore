7e547cd49c714bde063e02dcff2c18ba83c1df62
joshrossman <josh.rossman@gmail.com>
2025-04-24 18:57:33 -0400

    new

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import Auth0ProviderWithNavigate from './components/Logging/AuthProvider.tsx'
+
 
 
 const rootElement = document.getElementById('root')!;
