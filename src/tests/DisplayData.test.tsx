1544dde9add8f514261d2243a1a6ae9731ad3936
joshrossman <josh.rossman@gmail.com>
2025-05-14 16:58:10 -0400

    test

--- src/tests/DisplayData.test.tsx
+++ src/tests/DisplayData.test.tsx
@@ -6,7 +6,7 @@ describe('User Name', ()=>{
     test('Check value',()=>{
         const {getByText} = render(<DisplayData/>)
         const myData = getByText(/User List/i)
-        expect(myData.textContent).toBe('User List')
+        expect(myData.textContent).toBe('User List:')
         expect(myData.tagName).toBe('H2')
 
     })
